3219361bac7800298d93f9a7f5639e01fb6bb298
Nguyen <emailcuaban@gmail.com>
2026-04-20 10:00:00 +0700

    myutil

--- src/main/java/vn/fpoly/th04829_20042026/methods/MyUtil.java
+++ src/main/java/vn/fpoly/th04829_20042026/methods/MyUtil.java
@@ -1,4 +1,16 @@
 package vn.fpoly.th04829_20042026.methods;
 
 public class MyUtil {
+    public int tinhTongChan(int n){
+        if (n<=0){
+            throw new IllegalArgumentException("n khong hop le");
+        }
+        int tong = 0;
+        for (int i = 0; i<=n;i++){
+            if (i%2==0){
+                tong+=i;
+            }
+        }
+        return tong;
+    }
 }
