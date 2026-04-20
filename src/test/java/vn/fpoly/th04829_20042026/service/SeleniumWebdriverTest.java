c8f78ece22f9fd129601fa1fea3924e32d3b637a
Nguyen <emailcuaban@gmail.com>
2026-04-20 09:59:20 +0700

    selenium

--- src/test/java/vn/fpoly/th04829_20042026/service/SeleniumWebdriverTest.java
+++ src/test/java/vn/fpoly/th04829_20042026/service/SeleniumWebdriverTest.java
@@ -1,4 +1,35 @@
 package vn.fpoly.th04829_20042026.service;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumWebdriverTest {
+    /*
+    WebDriver webDriver;
+    @BeforeEach
+    void setUp(){
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+    }
+    @Test
+    void testSteps() throws InterruptedException{
+        webDriver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
+        //click menu
+        webDriver.findElement(By.xpath("//*[@id=\"menuToggle\"]/input")).click();
+        //Sign In Portal
+
+Thread.sleep(2000);
+        webDriver.findElement(By.xpath("//*[@id=\"menu\"]/a[2]/li")).click();
+        //login
+        webDriver.findElement(By.xpath("//*[@id=\"usr\"]")).sendKeys("sa");
+        webDriver.findElement(By.xpath("//*[@id=\"pwd\"]")).sendKeys("sa");
+        webDriver.findElement(By.xpath("//*[@id=\"second_form\"]/input")).click();
+        webDriver.quit();
+    }
+
+     */
 }
